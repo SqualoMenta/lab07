7bf0c565d65f44f93dba7deb9e25411f2030c567
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2023-11-12 09:32:33 +0100

    Ex 72, MonthSortedNested completed

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,8 +1,6 @@
 package it.unibo.nestedenum;
 
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
@@ -11,11 +9,75 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(Month.fromString(o1).getDays(), Month.fromString(o2).getDays());
+            }
+
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return Month.fromString(o1).compareTo(Month.fromString(o2));
+            }
+
+        };
+    }
+
+    private enum Month {
+        JANUARY(31),
+        FEBRUARY(28),
+        MARCH(31),
+        APRIL(30),
+        MAY(31),
+        JUNE(30),
+        JULY(31),
+        AUGUST(31),
+        SEPTEMBER(30),
+        OCTOBER(31),
+        NOVEMBER(30),
+        DECEMBER(31);
+
+        private final int days;
+
+        Month(int days) {
+            this.days = days;
+        }
+
+        public int getDays() {
+            return days;
+        }
+
+        public static Month fromString(String month) {
+            String name;
+            Month out = JANUARY;
+            boolean initialized = false;
+            for (Month i : Month.values()) {
+                if (i.name().length() >= month.length()) {
+                    name = i.name().substring(0, month.length());
+                    if (name.equalsIgnoreCase(month)) {
+                        if (!initialized) {
+                            out = i;
+                            initialized = true;
+                        } else {
+                            throw new IllegalArgumentException("Invalid month string: " + month);
+                        }
+                    }
+                }
+            }
+            if (!initialized) {
+                throw new IllegalArgumentException("Invalid month string: " + month);
+            } else {
+                return out;
+            }
+        }
+
     }
 }
